3ff1b4d3686e3db61cd654ab24959807329af789
CORNELIA MAYER <cornelia.mayer@hak-bregenz.at>
2025-02-20 15:43:52 +0100

    Install Chakra UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), tsconfigPaths()],
     server: {
         port: 57919,
     }
