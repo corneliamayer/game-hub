2d3a9e6fecb06446657fd08841b155a2a33084dd
CORNELIA MAYER <cornelia.mayer@hak-bregenz.at>
2025-03-22 11:38:38 +0100

    Display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, Image } from "@chakra-ui/react";
+import { Card, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game
@@ -12,8 +13,11 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <Card.Body>
         <Card.Title fontSize='2xl'>{game.name}</Card.Title>
-        <PlatformIconList
-          platforms={game.parent_platforms.map(p => p.platform)} />
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map(p => p.platform)} />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </Card.Body>
     </Card.Root>
   );
