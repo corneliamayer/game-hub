b3be771da4fe026f6c05389a97b728a118d5936a
CORNELIA MAYER <cornelia.mayer@hak-bregenz.at>
2025-03-29 14:43:07 +0100

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -43,7 +44,8 @@ function App() {
       <HStack spaceX={3} paddingLeft={10} marginBottom={-2}>
         <PlatformSelector selectedPlatform={gameQuery.platform}
           onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
-        <SortSelector />
+        <SortSelector selectedSortOrder={gameQuery.sortOrder}
+          onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
       </HStack>
       <GameGrid gameQuery={gameQuery} />
     </GridItem>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,23 +1,38 @@
 import { Button, Menu, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  selectedSortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, selectedSortOrder }: Props) => {
+  const sortOrders = [
+    { value: '', label: 'Relevance' },
+    { value: '-added', label: 'Date added' },
+    { value: 'name', label: 'Name' },
+    { value: '-released', label: 'Release date' },
+    { value: '-metacritic', label: 'Popularity' },
+    { value: '-rating', label: 'Average Rating' }
+  ];
+
+  const currentSortOrder = sortOrders.find(order =>
+    order.value === selectedSortOrder);
+
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="outline" size="sm">
-          Order by: Relevance <BsChevronDown />
+          Order by: {currentSortOrder?.label || 'Relevance'} <BsChevronDown />
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            <Menu.Item value={"relevance"}>Relevance</Menu.Item>
-            <Menu.Item value={"date-added"}>Date added</Menu.Item>
-            <Menu.Item value={"name"}>Name</Menu.Item>
-            <Menu.Item value={"release-date"}>Release date</Menu.Item>
-            <Menu.Item value={"popularity"}>Popularity</Menu.Item>
-            <Menu.Item value={"average-rating"}>Average Rating</Menu.Item>
+            {sortOrders.map(order => 
+              <Menu.Item value={order.value} key={order.value}
+                onClick={() => onSelectSortOrder(order.value)}>
+                {order.label}</Menu.Item>)}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
